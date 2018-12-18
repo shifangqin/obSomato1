570067f20c3eb3dd2a29c438314d0a4523d37426
shifangqin <shifangqin@orbbec.com>
2018-12-18 14:06:35 +0800

    finish2

--- businessView/business_commonLogic.py
+++ businessView/business_commonLogic.py
@@ -10,55 +10,54 @@ from selenium.common.exceptions import NoSuchElementException
 import os
 import subprocess
 import re
-
-
-# import sys
+import sys
 # sys.setrecursionlimit(100000)
+
 """
+the roles
+·   元素By定义在方法外，在方法内找寻以及调用
+·   元素命名根据它的属性命名
+·   元素By定义时，命名的最后一个单词的首字母小写，找寻元素是首字母大写，如：
+·   id_soft_item_name&id_soft_item_Name
+·
 the goal scenario:
 1. check the app list and its' version
 2. check the APPs under each tab
 """
 #
 class CommonLogicValidation(CommonFunc):
-    # home page
+
+    text_view=(By.CLASS_NAME,'android.widget.TextView')
     rec_root = (By.XPATH, '//*[@text="推荐"]')
-    reco_sec_appRoot = (By.ID, 'com.orbbec.gdgamecenter:id/framelayout_img')
-    # rec_root = (By.ID, 'com.orbbec.gdgamecenter:id/home_item_tw_content')
-
-    #person center page
-    personApp_manager_Btn = (By.ID, 'com.orbbec.gdgamecenter:id/person_center_app_manager')
-    allUpdate_Btn = (By.ID, 'com.orbbec.gdgamecenter:id/all_update_btn')
-
-    soft_view_pager = (By.ID, 'com.orbbec.gdgamecenter:id/soft_viewpager')
-    soft_item_view=(By.ID,'com.orbbec.gdgamecenter:id/soft_item_view')
-    soft_item_name = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_name')
-
-    app_uninstall_btn = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_uninstall_fl')
-    app_uninstall_confir_btn = (By.ID, 'com.orbbec.gdgamecenter:id/confir_btn')
-    app_uninstall_cancel_btn = (By.ID, 'com.orbbec.gdgamecenter:id/cancel_btn')
-
-    # install page
-    install_Btn = (By.ID, 'com.orbbec.gdgamecenter:id/soft_down')
-    app_nameTV = (By.ID, 'com.orbbec.gdgamecenter:id/soft_name')
-    # ok_installBtn = (By.ID, 'com.android.packageinstaller:id/ok_button')
-    # launch_installBtn=(By.ID,'com.android.packageinstaller:id/launch_button')
-    # done_installBtn = (By.ID, 'com.android.packageinstaller:id/done_button')
     apps_all_GridV = (By.CLASS_NAME, 'android.widget.GridView')
 
+    id_frame_layout_img = (By.ID, 'com.orbbec.gdgamecenter:id/framelayout_img')
+    id_btn=(By.ID, 'com.orbbec.gdgamecenter:id/btn')
+    id_soft_down = (By.ID, 'com.orbbec.gdgamecenter:id/soft_down') #install page download
+    id_soft_name = (By.ID, 'com.orbbec.gdgamecenter:id/soft_name') #install page
 
-    app_name = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_name')
-    app_version = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_version')
-    app_size = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_size')
+    id_soft_view_pager = (By.ID, 'com.orbbec.gdgamecenter:id/soft_viewpager')
+    id_soft_item_view=(By.ID, 'com.orbbec.gdgamecenter:id/soft_item_view')
+    id_soft_item_name = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_name')
+    id_soft_item_version = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_version') # app_manager_page
+    id_soft_item_size = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_size') # app_manager_page
+
+    id_soft_item_uninstall_fl = (By.ID, 'com.orbbec.gdgamecenter:id/soft_item_uninstall_fl')
+    id_confir_btn = (By.ID, 'com.orbbec.gdgamecenter:id/confir_btn')
+    id_cancel_btn = (By.ID, 'com.orbbec.gdgamecenter:id/cancel_btn')
+
+    id_dialog_notices_root=(By.ID, 'com.orbbec.gdgamecenter:id/dialog_notices_root') # install spaceless
+    id_cancel_exit_btn = (By.ID, 'com.orbbec.gdgamecenter:id/cancel_exit_btn') #retain page
+    id_content_layout = (By.ID, 'com.orbbec.gdgamecenter:id/centent_layout') #retain page
+
+    id_person_center_app_manager = (By.ID, 'com.orbbec.gdgamecenter:id/person_center_app_manager')
+    id_all_update_btn = (By.ID, 'com.orbbec.gdgamecenter:id/all_update_btn')
+
+    # rec_root = (By.ID, 'com.orbbec.gdgamecenter:id/home_item_tw_content')
 
     """
-key events
-    """
-
-
-    """
-get to certain page
-    """
+ go to certain page 
+     """
     def confirm_on_homePge(self):
 
         try:
@@ -67,9 +66,10 @@ get to certain page
             print("=======what a pity, we aren't get to homepage!!=======")
         else:
             print('======well done,we are on homepage 推荐!!!!!=======')
-    def swip_to_secondTab(self):
+
+    def go_to_2ndTab(self):
         self.confirm_on_homePge()
-        self.adbKeyEvent(self.KEYCODE_DPAD_RIGHT,2)
+        self.KeyEvent(self.KEYCODE_DPAD_RIGHT, 2)
 
         tab_name = (By.XPATH, '//*[@text="益智教育"]')
         try:
@@ -78,9 +78,10 @@ get to certain page
             print("=======what a pity, we aren't get to 益智教育 tab!!=======")
         else:
             print('======well done,we are on the second tab 益智教育!!!!!=======')
-    def swip_to_thirdTab(self):
+
+    def go_to_3rdTab(self):
         self.confirm_on_homePge()
-        self.adbKeyEvent(self.KEYCODE_DPAD_RIGHT,3)
+        self.KeyEvent(self.KEYCODE_DPAD_RIGHT, 3)
 
         tab_name = (By.XPATH, '//*[@text="健康互动"]')
         try:
@@ -89,9 +90,10 @@ get to certain page
             print("=======what a pity, we aren't get to 健康互动 tab!!=======")
         else:
             print('======well done,we are on the third  tab 健康互动!!!!!=======')
-    def swip_to_fourthTab(self):
+
+    def go_to_4thTab(self):
         self.confirm_on_homePge()
-        self.adbKeyEvent(self.KEYCODE_DPAD_RIGHT, 4)
+        self.KeyEvent(self.KEYCODE_DPAD_RIGHT, 4)
 
         tab_name = (By.XPATH, '//*[@text="休闲娱乐"]')
         try:
@@ -100,77 +102,102 @@ get to certain page
             print("=======what a pity, we aren't get to 休闲娱乐 tab!!=======")
         else:
             print('======well done,we are on fourth tab闲休娱乐!!!!!=======')
-    def go_person_center(self):
+
+    def go_to_person_center(self):
         self.confirm_on_homePge()
-        self.adbKeyEvent(self.KEYCODE_DPAD_RIGHT, 5)
+        self.KeyEvent(self.KEYCODE_DPAD_RIGHT, 5)
 
 
         try:
-            self.find_element(*self.personApp_manager_Btn)
+            self.find_element(*self.id_person_center_app_manager)
         except TimeoutException:
             print("=======what a pity, we aren't get to 个人中心 tab!!=======")
         else:
             print('======well done,we are on 个人中心 tab!!!!!=======')
-    def go_to_app_managePage(self):
-        self.go_person_center()
 
-        self.find_element(*self.personApp_manager_Btn).click()
+    def go_to_app_managePage(self):
+        self.go_to_person_center()
+
+        self.find_element(*self.id_person_center_app_manager).click()
         sleep(5)
 
         try:
-            self.find_element(*self.allUpdate_Btn)
+            self.find_element(*self.id_all_update_btn)
         except NoSuchElementException:
             print("=======what a pity, we aren't get to 应用管理 页!!=======")
         else:
             print('======well done,we are on 应用管理 页!!!!!=======')
+
     """
 apps installation 
     """
     def install_certain_app(self):
         self.confirm_on_homePge()
 
-        self.find_element(*self.reco_sec_appRoot).click()
-        self.KEYCONDE_CONFIRM()
+        self.find_element(*self.id_frame_layout_img).click()
+        self.KeyEvent(self.KEYCODE_ENTER)
         sleep(3)
 
         try:
-            app_installBtn=self.find_element(*self.install_Btn)
+            app_installBtn=self.find_element(*self.id_soft_down)
         except NoSuchElementException:
             print("此应用已下载")
         else:
 
             app_installBtn.click()
             print("请注意，有个盒子可能需要手动点击下载")
+
     def uninstall_certain_app(self,appname):
         self.go_to_app_managePage()
-        app_soft_viewPge=self.find_element(*self.soft_view_pager)
-        app_soft_views=app_soft_viewPge.find_elements(*self.soft_item_view)
+        app_soft_viewPge=self.find_element(*self.id_soft_view_pager)
+        app_soft_views=app_soft_viewPge.find_elements(*self.id_soft_item_view)
         for views in app_soft_views :
-            name=views.find_element(*self.soft_item_name).text
-            uninstall_btn=views.find_element(self.app_uninstall_btn)
+            name=views.find_element(*self.id_soft_item_name).text
+            uninstall_btn=views.find_element(self.id_soft_item_uninstall_fl)
             if appname==name():
                 uninstall_btn.click()
                 print("请留意，有可能需要你的点击")
             break
+
     def uninstall_all_app(self):
         self.go_to_app_managePage()
-        app_soft_viewPge=self.find_element(*self.soft_view_pager)
-        app_uninstall_btns=app_soft_viewPge.find_elements(*self.app_uninstall_btn)
+        app_soft_viewPge=self.find_element(*self.id_soft_view_pager)
+        app_uninstall_btns=app_soft_viewPge.find_elements(*self.id_soft_item_uninstall_fl)
         for app in app_uninstall_btns:
             try:
-                app_uninstall_Btn = self.find_element(*self.app_uninstall_btn)
+                app_uninstall_Btn = self.find_element(*self.id_soft_item_uninstall_fl)
             except NoSuchElementException:
                 print("没有找到卸载按钮")
             else:
                 app_uninstall_Btn.click()
                 sleep(3)
                 try:
-                    app_uninstall_Btn = self.find_element(*self.app_uninstall_confir_btn)
+                    app_uninstall_Btn = self.find_element(*self.id_confir_btn)
                 except NoSuchElementException:
                     print("卸载二次弹窗没有找到")
                 else:
                     app_uninstall_Btn.click()
                     print("请留意，有可能需要你的点击")
+
+    def install_insufficient_notice(self):
+        """
+
+        """
+        try:
+            space_insufficient_notice_Root = self.find_element(*self.id_dialog_notices_root)
+        except NoSuchElementException:
+            print("=====oops! can't find notice dialog=======")
+        else:
+            space_insufficient_notice_TextView = space_insufficient_notice_Root.find_element(*self.text_view)
+            notice_text=space_insufficient_notice_TextView[0].text # here are three text views
+
+            if notice_text=='存储空间不足':
+                print('well done RIGHT notice text:',notice_text)
+            else:
+                print("=====oops! illegal notice text=======")
+
+            space_insufficient_notice_known_btn =space_insufficient_notice_Root.find_element(*self.id_btn)
+            space_insufficient_notice_known_btn.click()
 
 
     """
@@ -190,25 +217,27 @@ person Center
             rowNum+=1
             rowNum_pointer = self.get_csv_data(csv_file, rowNum)
         return app_input_set
+
     def get_app_info_from_personCenterPge(self):
         self.go_to_app_managePage()
 
 
 
         apps_all_GV = self.find_element(*self.apps_all_GridV)
-        app_soft_IV = apps_all_GV.find_elements(*self.soft_item_view)
+        app_soft_IV = apps_all_GV.find_elements(*self.id_soft_item_view)
 
         app_pge_set = set()
         app_sizeDic={}
         for gameParent in app_soft_IV:
-            name = gameParent.find_element(*self.app_name).text
-            version = gameParent.find_element(*self.app_version).text
+            name = gameParent.find_element(*self.id_soft_item_name).text
+            version = gameParent.find_element(*self.id_soft_item_version).text
             versionStr = 'v' + version[3:]
-            size = gameParent.find_element(*self.app_size).text
+            size = gameParent.find_element(*self.id_soft_item_size).text
             app_sizeDic[name]=size
             app_tuple = (name, versionStr)
             app_pge_set.add(app_tuple)
         return app_pge_set,app_sizeDic
+
     def check_appList_meet_req(self, app_input_set, app_pge_set):
 
         if app_input_set.difference(app_pge_set)==set():
@@ -220,52 +249,24 @@ person Center
             print("没有多出应用:")
         else:
             print("多出如下应用：",app_pge_set.difference(app_input_set))
-    """
-retain page
-    """
+
     def get_game_name_from_retainPg(self):
-
-        exitBtn = (By.ID, 'com.orbbec.gdgamecenter:id/cancel_exit_btn')
-        game_content = (By.ID, 'com.orbbec.gdgamecenter:id/centent_layout')
-        game_names = (By.CLASS_NAME, 'android.widget.TextView')
-
         name_list = []
 
-        # confirm it is on home page, and then send "return" key
-
         self.confirm_on_homePge()
-        self.keyEvents_return(1)
+        self.KeyEvent(self.KEYCODE_BACK)
 
         """
         get the name list of the random recommended games
         """
-        game_content = self.find_element(*game_content)
-        game_names = game_content.find_elements(*game_names)
+        game_content = self.find_element(*self.id_content_layout)
+        game_names = game_content.find_elements(*self.id_soft_item_name)
 
         for name in game_names:
             name_list.append(name.text)
-        self.find_element(*exitBtn).click()
+        self.find_element(*self.id_cancel_exit_btn).click()
         sleep(10)
         return name_list
-
-    def inject_certain_trash(self):
-        cmd1 = 'adb shell df'
-
-        p = subprocess.Popen(cmd1, stdout=subprocess.PIPE, shell=True)
-        out = p.communicate()[0]
-        size, userd, fsize = re.findall(r'/data\s+(\d+\.\d+)G\s+(\d+\.\d+)G\s+(\d+\.\d+)G', str(out))[0]
-        free_size=float(fsize)
-
-        bsNum = 102400 #100Kb
-        countNum=int ((free_size * 1048576)%bsNum) # 换算成百KB
-
-
-        # cmd2='adb shell'
-        # os.system(cmd2)
-        # os.chdir('/data/data')
-
-        cmd3 ='adb shell dd if=/dev/zero of=/data/data/test.zip bs=%s count=%s'%(bsNum,countNum)
-        os.system(cmd3)
 
 
 
@@ -276,22 +277,8 @@ if __name__ == '__main__':
     driver=desired_caps()
 
     Intial=CommonLogicValidation(driver)
-    # Intial.swip_to_secondTab()
-    Intial.inject_certain_trash()
-    # Intial.install_certain_app()
-    # Intial.getScreenShot('Insufficient Tip')
-
-
-
-
-
-    """
-    check retain page app appear in random
-    """
-    # list1 = intialPara.get_game_name_from_retainPg()
-    #
-    # list2 = intialPara.get_game_name_from_retainPg()
-    # if list1 == list2:
-    #     print("不是随机刷新")
-    # else:
-    #     print("随机刷新")
+    Intial.go_to_2ndTab()
+    # Intial.go_to_3rdTab()
+    # Intial.go_to_4thTab()
+    # Intial.go_to_person_center()
+    # Intial.go_to_app_managePage()
--- common/common_func.py
+++ common/common_func.py
@@ -1,3 +1,4 @@
+#coding:'UTF-8'
 from baseView.baseView import BaseView
 from selenium.webdriver.common.by import By
 import logging
@@ -10,21 +11,25 @@ from time import sleep
 
 class CommonFunc(BaseView):
     # 常用keyevent事件
+    KEYCODE_BACK = 4
+    KEYCODE_ENTER = 66
     KEYCODE_HOME=3
     KEYCODE_MENU=82
-    KEYCODE_BACK=4
+
     KEYCODE_POWER=26
     KEYCODE_DPAD_UP=19
     KEYCODE_DPAD_DOWN=20
     KEYCODE_DPAD_LEFT=21
     KEYCODE_DPAD_RIGHT=22
 
-    def adbKeyEvent(self, Key_code, num_of_acts):
+
+    def KeyEvent(self, Key_code, num_of_acts=1):
         while num_of_acts:
             adb ='adb shell input keyevent %s'%Key_code
             os.system(adb)
             num_of_acts-=1
         sleep(3)
+
     def getTime(self):
         self.now=time.strftime("%Y-%m-%d %H_%M_%S")
         return self.now
@@ -53,6 +58,24 @@ class CommonFunc(BaseView):
         size, userd, fsize = re.findall(r'/data\s+(\d+\.\d+)G\s+(\d+\.\d+)G\s+(\d+\.\d+)G', str(out))[0]
 
         return size, userd, fsize
+
+    def inject_certain_size_trash(self):
+        cmd1 = 'adb shell df'
+
+        p = subprocess.Popen(cmd1, stdout=subprocess.PIPE, shell=True)
+        out = p.communicate()[0]
+        size, userd, fsize = re.findall(r'/data\s+(\d+\.\d+)G\s+(\d+\.\d+)M\s+(\d+\.\d+)G', str(out))[0]
+        free_size=float(fsize)
+
+        bsNum = 102400 #100Kb
+        countNum=int ((free_size * 1048576)%bsNum) # 换算成百KB
+
+        cmd3 ='adb shell dd if=/dev/zero of=/data/data/test.zip bs=%s count=%s'%(bsNum,countNum)
+        os.system(cmd3)
+
+    def remove_test_trash(self):
+        adb='adb shell rm /data/data/test.zip'
+        os.system(adb)
 
 
 
